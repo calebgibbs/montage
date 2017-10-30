53225a103653485b7dead5a0e2509b8c0eaba166
Caleb Gibbs <calebgibbs@Calebs-MBP.hub>
2017-10-30 14:18:44 +1300

    styling elements on home page

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     $('img').on('dragstart', function(event) { event.preventDefault(); });
     $('.drop-trigger').hover(function(){
-    	$('#drop-down').css({'opacity':'1'});   
+    	$('#drop-down').addClass('show-menu');   
     }); 
     $('.menu-item').hover(function(){
-    	$('#drop-down').css({'opacity':'0'});
+    	$('#drop-down').removeClass('show-menu');
     }); 
-    $('body').hover(function(){
-    	$('#drop-down').css({'opacity':'0'});
+    $('.body').hover(function(){
+    	$('#drop-down').removeClass('show-menu');
     }); 
 
     //mobile 
     $('.nav-icon').on('click',function() {
-    //animate button
-    $(this).toggleClass('active');   
-    //show menu  
-    $('.main-menu').toggleClass('menu-active');
+    	//animate button
+    	$(this).toggleClass('active');   
+    	//show menu  
+    	$('#menu').toggleClass('menu-active');
 	});
 });
