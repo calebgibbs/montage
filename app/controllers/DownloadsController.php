73f1480504f0f1a61c374509507c798e5977482d
Caleb Gibbs <calebgibbs@me.com>
2018-04-24 11:38:14 +1200

    Downloads

--- app/controllers/DownloadsController.php
+++ app/controllers/DownloadsController.php
@@ -18,20 +18,43 @@ class DownloadsController extends PageController {
 		$this->data['title'] = 'Downloads'; 
 		$this->data['class'] = 'downloads-title'; 
 
-		$sql = "SELECT p.id, p.title, 
-					i.product_id, i.image, i.image_position, 
-					d.poduct_id, d.title 
-				FROM products AS p 
-				JOIN downloads AS d ON d.product_id = p.id";
-		die($sql);
-		$result = $this->dbc->query($sql); 
+		$sql = "SELECT product_id FROM downloads"; 
+		$results = $this->dbc->query($sql); 
+		if($results->num_rows != 0){ 
+			$results = $results->fetch_all(MYSQLI_ASSOC); 
+			
+			$prodStr = "";
+			$products = array();
 
-		if (!$result || $result->num_rows == 0) {
-			$this->data['results'] = "No results";
-		}else{
-			$this->data['results'] = $result->fetch_all(MYSQLI_ASSOC);
-		} 
+			foreach( $results as $result ){ 
+				$prod = $result['product_id'];
+				if( !in_array($prod, $products) ){ 
+					array_push($products, $prod); 
+					$prodStr .= $prod.', ';
+				}
+			} 
+
+			$ids = trim($prodStr); $ids = substr($ids, 0, -1);
+
+			$sql = "SELECT p.id, p.title AS score_title, p.category, p.category2, p.supplier, 
+			i.product_id, i.image, i.image_position 
+			FROM products AS p 
+			JOIN product_images AS i 
+			ON p.id = i.product_id
+			WHERE 
+			p.id IN ($ids) 
+			AND image_position = '1'";   
+			
+			$result = $this->dbc->query($sql); 
+
+			if (!$result || $result->num_rows == 0) {
+				$this->data['results'] = "No results";
+			}else{
+				$this->data['results'] = $result->fetch_all(MYSQLI_ASSOC);
+			}
+
+		}
 
 		
 	}
-}
+} 
